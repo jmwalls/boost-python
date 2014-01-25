ec711dc1e8b42462b4c60afafd37513c9ddbfdd0
Jeff Walls <jwalls9@gmail.com>
2014-01-25 13:59:05 -0500

    added numpy c-api calls to a new data object

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 ##Boost python testing
 -----
 
-Simple test for boost python.
+Simple test for boost python. Tests the following functionality:
+
+* call a function (greet ())
+* create a basic object and set some primitive data (World ())
+* create an object that can receive and return numpy arrays (Data ())
 
 Build
 ----
@@ -19,13 +23,39 @@ From a python interpreter (within the build dir):
 
 In [1]: import libpyexamples  
 
-In [2]: w = libpyexamples.World ()  
+In [2]: libpyexamples.greet ()  
+Out[2]: 'Hello, World!'  
 
-In [3]: w.greet ()  
-Out[3]: 'Hello, World!'  
+In [3]: w0 = libpyexamples.World ()  
 
-In [4]: w.set ('new message')  
+In [4]: w0.greet ()  
+Out[4]: 'Hello, World!'  
 
-In [5]: w.greet ()  
-Out[5]: 'new message'  
+In [5]: w0.set ('new hello message')  
+
+In [6]: w0.greet ()  
+Out[6]: 'new hello message'  
+
+In [7]: w1 = libpyexamples.World ('hello init message')  
+
+In [8]: w1.greet ()  
+Out[8]: 'hello init message'  
+
+In [9]: import numpy as np  
+
+In [10]: a = np.arange (5, dtype='float')  
+
+In [11]: d = libpyexamples.Data ()  
+
+In [12]: d.set (a)  
+
+In [13]: b = d.squared ()  
+In [14]: b  
+Out[14]: array([  0.,   1.,   4.,   9.,  16.])  
+
+Notes
+-----
+
+The Data object is particularly brittle---there are no real checks right now.
+The array based to it *must* be of double type and only a single dimension.
 
--- pyexamples.cpp
+++ pyexamples.cpp
@@ -1,5 +1,13 @@
+#include <iostream>
+#include <vector>
+
 #include <Python.h>
+#include <numpy/ndarraytypes.h>
+#include <numpy/ndarrayobject.h>
+
 #include <boost/python.hpp>
+
+using namespace std;
 
 const char* greet ()
 {
@@ -8,14 +16,56 @@ const char* greet ()
 
 struct World
 {
-    World () : _msg ("Hello, World!") {}
-    World (std::string msg) : _msg (msg) {}
+  World () : _msg ("Hello, World!") {}
+  World (string msg) : _msg (msg) {}
 
-    void set (std::string msg) {_msg = msg;}
-    std::string greet () {return _msg;}
+  void set (string msg) {_msg = msg;}
+  string greet () {return _msg;}
 
-    std::string _msg;
+  string _msg;
 };
+
+
+class Data
+{
+public:
+  Data () {import_array ();}
+  ~Data () {}
+
+  void set (PyObject *a)
+  {
+      PyArrayObject *arr = (PyArrayObject*) a;
+      assert (arr->nd==_ndims);                      // should be 1d here
+      assert (arr->descr->type_num==PyArray_DOUBLE); // should be double...
+
+      _data.clear ();
+      _data.resize (arr->dimensions[0]);
+
+      for (int i=0;i<arr->dimensions[0];++i) {
+          double *d = (double*) (arr->data + i*arr->strides[0]);
+          _data[i] = *d;
+      }
+  }
+
+  PyObject* squared ()
+  {
+      PyArrayObject *arr;
+
+      int dims[1] = {static_cast<int> (_data.size ())};
+      arr = (PyArrayObject*) PyArray_FromDims (_ndims, dims, PyArray_DOUBLE);
+
+      double *sqd = new double[dims[0]];
+      for (int i=0;i<dims[0];++i) sqd[i] = _data[i]*_data[i];
+      arr->data = (char*) sqd;
+
+      return PyArray_Return (arr);
+  }
+
+private:
+  vector<double> _data;
+  static const int _ndims = 1;
+};
+
 
 BOOST_PYTHON_MODULE (libpyexamples)
 {
@@ -23,8 +73,13 @@ BOOST_PYTHON_MODULE (libpyexamples)
     def ("greet", greet);
 
     class_<World>("World")
-        .def (init<std::string> ())
+        .def (init<string> ())
         .def ("greet", &World::greet)
         .def ("set", &World::set)
+    ;
+
+    class_<Data>("Data")
+        .def ("set", &Data::set)
+        .def ("squared", &Data::squared)
     ;
 }
